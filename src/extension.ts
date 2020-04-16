890a433581e412a5f7466b9c547ea585298074f9
adershmanoj <adershmanoj@gmail.com>
2020-04-16 17:31:09 +0530

    Moved save register command to CheckPointExplorer from CheckPointProvider

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import {CheckPointExplorer} from './CheckPointExplorer'
+import {CheckPointExplorer} from './CheckPointExplorer';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
