906a9047d5ea5b5844b7f580223e9ace187a5826
adershmanoj <adershmanoj@gmail.com>
2020-04-21 19:42:53 +0530

    Edited comments for Interface

--- src/Interfaces/CheckPointInterfaces.ts
+++ src/Interfaces/CheckPointInterfaces.ts
@@ -3,7 +3,7 @@ import {patch_obj} from 'diff-match-patch';
 /**
 	 * @member patches Provides a patches array which
 	 * will be used to represent file patches.
-	 *  @member timestamps An array of Dates which will be used to store the file change times and,
+	 * @member timestamps An array of Dates which will be used to store the file change times and,
      * @member current String to store the current value of file
 	 */
 export interface CheckPointObject {
@@ -12,7 +12,7 @@ export interface CheckPointObject {
 	current: string
 }
 /**
-	 *  @member timestamp Date for the current checkpoint,
+	 * @member timestamp Date for the current checkpoint,
      * @member index Checkpoint index
 	 */
 export interface CheckPointTreeItem {
